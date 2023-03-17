505b226a26ad443ce36293500097017450879de6
Nbiba Bedis <bedisnbiba@gmail.com>
2023-03-17 20:39:27 +0100

    sw

--- static/pwa/app.js
+++ static/pwa/app.js
@@ -1,7 +1,7 @@
 if ("serviceWorker" in navigator) {
   addEventListener("load",async function () {
     navigator.serviceWorker
-      .register("/static/serviceWorker.js")
+      .register("/serviceWorker.js")
       .then(() => console.log("service worker registered"))
       .catch((err) => console.log("service worker not registered", err));
   });

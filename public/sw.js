bd2da3e7a6057d29e2e58beb3bd14b6c12f481dc
Nbiba Bedis <bedisnbiba@gmail.com>
2024-08-31 08:20:20 +0100

    update sw

--- public/sw.js
+++ public/sw.js
@@ -1,36 +1,24 @@
 // Cache requests so the app can work offline
-// Uncomment to enable caching
 // https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Offline_Service_workers
-const cacheName = "dateDiff-v11";
+const cacheName = "myapp";
 
 self.addEventListener("fetch", (e) => {
   e.respondWith(
     (async () => {
-      const r = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (r) {
-        return r;
+      try {
+        const response = await fetch(e.request);
+        const cache = await caches.open(cacheName);
+        console.log(`[Service Worker] Caching resource: ${e.request.url}`);
+        cache.put(e.request, response.clone());
+        return response;
+      } catch {
+        // if we have no network access, try to use the cache
+        const response = await caches.match(e.request);
+        console.log(`[Service Worker] Using cached resource: ${e.request.url}`);
+        if (response) {
+          return response;
+        }
       }
-      const response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
     })(),
-  );
-});
-
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key === cacheName) {
-            return;
-          }
-          return caches.delete(key);
-        }),
-      );
-    }),
   );
 });
